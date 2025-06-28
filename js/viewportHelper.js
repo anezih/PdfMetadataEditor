dda3560d276ca8e9a77bfea697f761fae5d49270
anezih <90565940+anezih@users.noreply.github.com>
2025-06-28 15:38:45 +0000

    Deploying to gh-pages from @ anezih/PdfMetadataEditor@4f48bc295b16dd0f5dd49b905fe155eb236783b3 🚀

---
+++ js/viewportHelper.js
@@ -0,0 +1,14 @@
+﻿var _objRef;
+
+window.ViewPortChanges = {
+    GetDims: function (objRef) {
+        _objRef = objRef;
+        _objRef.invokeMethodAsync('SetViewPortDimensions', window.innerWidth, window.innerHeight);
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (_objRef != null) {
+        _objRef.invokeMethodAsync('SetViewPortDimensions', window.innerWidth, window.innerHeight);
+    }
+});
