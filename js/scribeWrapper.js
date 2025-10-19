d0220a2050cafc2023a8246f86b94e32aa760181
anezih <90565940+anezih@users.noreply.github.com>
2025-10-19 16:55:33 +0000

    Deploying to gh-pages from @ anezih/PdfMetadataEditor@05da7ec34d72ff1f0fda1ecae365b186ac8dc864 🚀

---
+++ js/scribeWrapper.js
@@ -0,0 +1,34 @@
+﻿import scribe from '../scribejs/scribe.js'
+
+export function getScribe() {
+    scribe.opt.displayMode = "invis";
+    return scribe;
+}
+
+function ToArrayOfArrayBuffer(data) {
+    let buffer;
+
+    if (data instanceof ArrayBuffer)
+    {
+        buffer = data;
+    }
+    else if (ArrayBuffer.isView(data))
+    {
+        buffer = data.buffer;
+    }
+    else
+    {
+        throw new TypeError("Expected byte[] like object.");
+    }
+
+    return [buffer];
+}
+
+export function ImportPdf(pdfData) {
+    let pdf = ToArrayOfArrayBuffer(pdfData);
+
+    const SortedInputFiles = {
+        pdfFiles: pdf,
+    };
+    scribe.importFiles(SortedInputFiles);
+}
